4bad575555f9f2b92415b637eed46bf40fc94bd2
patriciomirosh <71236425+patriciomirosh@users.noreply.github.com>
2020-12-03 19:49:42 -0300

    Update app.js
    All the requests was checked, the program function well, all the coments explains his each respectively component

--- nodejs/app.js
+++ nodejs/app.js
@@ -10,51 +10,43 @@ const app = express();
 app.use(bodyParser.json());
 
 // MySql
+
 const connection = mysql.createConnection({
+  multipleStatements: true,
   host: 'localhost',
   user: 'root',
-  password: '',
+  Password: '',
   database: 'node20_mysql'
 });
 
-// Create the table and the first Log
-app.post('/create', (req, res) => {
-  const sql = 'CREATE TABLE `node20_mysql`.`admper`(ID INT NOT NULL  AUTO_INCREMENT, Concepto TEXT NOT NULL , Monto FLOAT NOT NULL , Fecha DATE NOT NULL , Tipo VARCHAR NOT NULL ,PRIMARY KEY (ID))';
-  connection.query(sql, (error, results) => {
-    if (error) throw error;
-    if (results.length > 0) {
-      res.json(results);
-    } else {
-      res.send('Not result');
-    }
-  });
-});
-//Put the first Row in the table 
-app.post('/first', (req, res) => {
+ 
+ 
 
-  const sql = 'INSERT INTO admper SET ?';
+//Put the first Row in the table Checked 
+app.post('/first/:Username', (req, res) => {
+  const {Username} = req.params
+  const sql = `INSERT INTO ${Username} SET ?`;
+  
   const registerObj1 = {
 
-    Concepto: "UltimoBalance",
-    Monto: 1500,
-    Fecha: "",
-    Tipo: "in"
+    Concepto: req.body.Concepto,
+    Monto: req.body.Monto,
+    Fecha: req.body.Fecha,
+    Tipo: req.body.Tipo
   };
 
   connection.query(sql, registerObj1, error => {
-    if (error) throw error;
+    if (error) throw error; 
     res.send('Error');
-  });
+  }); 
 })
+  
 
-// Route
-app.get('/', (req, res) => {
-  res.send('Welcome to my API!');
-});
 
-// last 10 logs in the table
-app.get('/last10', (req, res) => {
-  const sql = 'SELECT * FROM admper ORDER BY ID DESC LIMIT 10';
+// last 10 logs in the table Checked
+app.get('/last10/:Username', (req, res) => {
+  const {Username} = req.params;
+  const sql = `SELECT * FROM ad${Username} ORDER BY ID DESC LIMIT 10`;
 
   connection.query(sql, (error, results) => {
     if (error) throw error;
@@ -65,10 +57,11 @@ app.get('/last10', (req, res) => {
     }
   });
 });
-//Show all the Logs of the Table.
-app.get('/get', (req, res) => {
-  const sql = 'SELECT * FROM admper';
-
+//Show all the Logs of the Table. Checked
+app.get('/get/:Username', (req, res) => {
+  const {Username} = req.params;
+  const sql = `SELECT * FROM ad${Username}`;
+  
   connection.query(sql, (error, results) => {
     if (error) throw error;
     if (results.length > 0) {
@@ -78,10 +71,11 @@ app.get('/get', (req, res) => {
     }
   });
 });
-// obtain By ID
-app.get('/get/:id', (req, res) => {
+// Bbtain By ID Checked
+app.get('/get/:Username/:id', (req, res) => {
+  const {Username} = req.params;  
   const { id } = req.params;
-  const sql = `SELECT * FROM admper WHERE id = ${id}`;
+  const sql = `SELECT * FROM ${Username} WHERE id = ${id}`;
   connection.query(sql, (error, result) => {
     if (error) throw error;
 
@@ -92,10 +86,13 @@ app.get('/get/:id', (req, res) => {
     }
   });
 });
-// Get the sum of all entry logs
-app.get('/balance', (req, res) => {
-  const sql = 'SELECT id,sum(Monto) as suma FROM `admper` WHERE Tipo="in"';
+// Get the sum of all entry logs Checked
+app.get('/balance/:Username', (req, res) => {
+  const {Username} = req.params;
+  const sql = `SELECT id,sum(Monto) as suma FROM ad${Username} WHERE Tipo = "in" `;
+  
   connection.query(sql, (error, result) => {
+  
     if (error) throw error;
 
     if (result.length > 0) {
@@ -105,9 +102,11 @@ app.get('/balance', (req, res) => {
     }
   });
 });
-// Get the sum of all egress logs
-app.get('/balanceEg', (req, res) => {
-  const sql = 'SELECT id,sum(Monto) as suma FROM `admper` WHERE Tipo="eg"';
+// Get the sum of all egress logs Checked
+app.get('/balanceEg/:Username', (req, res) => {
+  const {Username} = req.params;
+  const sql =  `SELECT id,sum(Monto) as suma FROM ad${Username} WHERE Tipo = "eg" `;
+  
   connection.query(sql, (error, result) => {
     if (error) throw error;
 
@@ -119,9 +118,10 @@ app.get('/balanceEg', (req, res) => {
   });
 })
 
-//add a new log 
-app.post('/add', (req, res) => {
-  const sql = 'INSERT INTO admper SET ?';
+//add a new log  Checked
+app.post('/add/:Username', (req, res) => {
+  const {Username} = req.params;
+  const sql = `INSERT INTO ad${Username} SET ?`;
 
   const registerObj = {
 
@@ -131,16 +131,19 @@ app.post('/add', (req, res) => {
     Tipo: req.body.Tipo
   };
 
+
+
   connection.query(sql, registerObj, error => {
     if (error) throw error;
     res.send('Register created!');
   });
 });
-// update a Logs by ID
-app.put('/update/:id', (req, res) => {
+// update a Logs by ID Checked
+app.put('/update/:Username/:id', (req, res) => {
+  const { Username} =req.params;
   const { id } = req.params;
-  const { Concepto, Monto, Fecha } = req.body;
-  const sql = `UPDATE admper SET Concepto = '${Concepto}', Monto='${Monto}',Fecha='${Fecha}' WHERE id =${id}`;
+  const { Concepto, Monto , Fecha}= req.body;
+  const sql = `UPDATE ad${Username} SET Concepto = '${Concepto}', Monto='${Monto}',Fecha='${Fecha}' WHERE id =${id}`;
 
   connection.query(sql, error => {
     if (error) throw error;
@@ -148,16 +151,105 @@ app.put('/update/:id', (req, res) => {
   });
 });
 
-// delete logs by ID
-app.delete('/delete/:id', (req, res) => {
+// delete logs by ID Checked
+app.delete('/delete/:Username/:id', (req, res) => {
+  const { Username} =req.params;
   const { id } = req.params;
-  const sql = `DELETE FROM admper WHERE id= ${id}`;
+  const sql = `DELETE FROM ad${Username} WHERE id= ${id}`;
 
   connection.query(sql, error => {
     if (error) throw error;
     res.send('Delete Register');
   });
 });
+
+//Create table register checked 
+app.post('/createtableregister', (req, res) => {
+  const sql = 'CREATE TABLE node20_mysql.tableregister (ID INT NOT NULL  AUTO_INCREMENT, Username VARCHAR(50) NOT NULL , Mail VARCHAR(50) NOT NULL , Password VARCHAR(50) NOT NULL  ,PRIMARY KEY (ID))';
+  connection.query(sql, (error, results) => {
+    if (error) throw error;
+    if (results.length > 0) {
+      return res.json(results);
+    } else {
+      return res.send('Table created');
+    }
+  });
+}); 
+
+//Create table Username checked 
+app.post('/create/:Username', (req, res) => {
+  const { Username } = req.params;  
+  const sql = `CREATE TABLE node20_mysql.ad${Username} (ID INT NOT NULL  AUTO_INCREMENT, Concepto VARCHAR(50) NOT NULL , Monto FLOAT NOT NULL , Fecha DATE NOT NULL , Tipo VARCHAR(50) NOT NULL ,PRIMARY KEY (ID))`;
+  connection.query(sql, (error, results) => {
+    if (error) throw error;
+    if (results.length > 0) {
+      return res.json(results);
+    } else {
+      return res.send('Table created');
+    } 
+  });
+});
+
+
+//Create Username checked
+app.post('/newUser', (req, res) => {
+ 
+  const sql =  `INSERT INTO tableregister SET ?` ;
+
+  const registerObj = {
+
+    Username: req.body.Username,
+    Mail: req.body.Mail,
+    Password: req.body.Password, 
+  };
+  
+  connection.query(sql, registerObj, error => {
+    if (error) throw error;
+    return res.send('User not created!');
+  });  
+  
+}) 
+//Create fist Column of the new User Table, Checked
+app.post('/newUser/:Username', (req, res) => {
+  const { Username } = req.params;  
+  const sql =  `INSERT INTO ${Username} SET ?` ;
+
+  const registerObj = {
+
+    Concepto: req.body.Concepto,
+    Monto: req.body.Monto,
+    Fecha: req.body.Fecha,
+    Tipo: req.body.Tipo
+  };
+ 
+  connection.query(sql, registerObj, error => {
+    if (error) throw error;
+    return res.send('User not created!');
+  });  
+  
+}) 
+
+
+
+
+// Check Users and PassWord, Checked
+app.get('/PassWord/:Username', (req, res) => {
+  const {Username} = req.params;
+  const sql = `SELECT Username, Password FROM tableregister  `;
+  
+  connection.query(sql, (error, result) => {
+  
+    if (error) throw error;
+
+    if (result.length > 0) {
+      res.json(result);
+    } else {
+      res.send('No Register');
+    }
+  });
+});
+
+
 
 // Check connect
 connection.connect(error => {
@@ -166,6 +258,3 @@ connection.connect(error => {
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
-
